07f705df3038f25aa27743c86a4249f8f089965e
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2020-12-02 12:39:39 -0600

    Add root solution display & refactor day1 output

---
+++ src/aoc2020/core.clj
@@ -0,0 +1,20 @@
+(ns aoc2020.core
+  (:require [aoc2020.day1.core :as day1]
+            [aoc2020.day2.core :as day2]))
+
+(comment
+  day1/part1
+  ;; => {:x 21, :y 1999, :result 41979}
+
+  day1/part2
+  ;; => {:x 277, :y 624, :z 1119, :result 193416912}
+
+  day2/part1
+  ;; => 569
+
+  day2/part2
+  ;; => 346
+
+
+  0)
+
--- src/aoc2020/day1/core.clj
+++ src/aoc2020/day1/core.clj
@@ -8,19 +8,21 @@
                 (mapv #(Integer/parseInt %))))
 
 (def part1
-  (filter #(not (nil? %))
-          (for [x (set input)
-                y (set (filter #(<= (- 2020 x) %) input))]
-            (if (= 2020 (+ x y))
-              [x y (* x y)]))))
+  (first
+    (filter #(not (nil? %))
+            (for [x (set input)
+                  y (set (filter #(<= (- 2020 x) %) input))]
+              (if (= 2020 (+ x y))
+                {:x x :y y :result (* x y)})))))
 
 (def part2
-  (filter #(not (nil? %))
-          (for [x (set input)
-                y (set (filter #(<= % (- 2020 x)) input))
-                z (set (filter #(<= % (- 2020 x y)) input))]
-            (if (= 2020 (+ x y z))
-              [x y z (* x y z)]))))
+  (first
+    (filter #(not (nil? %))
+            (for [x (set input)
+                  y (set (filter #(<= % (- 2020 x)) input))
+                  z (set (filter #(<= % (- 2020 x y)) input))]
+              (if (= 2020 (+ x y z))
+                {:x x :y y :z z :result (* x y z)})))))
 
 (comment
   input
