fabc19afe92c53c5039a610be2ea65b110d0feb8
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2020-12-03 22:54:59 -0600

    Finish Day 3

--- src/aoc2020/core.clj
+++ src/aoc2020/core.clj
@@ -1,20 +1,23 @@
 (ns aoc2020.core
   (:require [aoc2020.day1.core :as day1]
-            [aoc2020.day2.core :as day2]))
+            [aoc2020.day2.core :as day2]
+            [aoc2020.day3.core :as day3]))
 
 (comment
   day1/part1
   ;; => {:x 21, :y 1999, :result 41979}
-
   day1/part2
   ;; => {:x 277, :y 624, :z 1119, :result 193416912}
 
   day2/part1
   ;; => 569
-
   day2/part2
   ;; => 346
 
+  day3/part1
+  ;; => 223
+  day3/part2
+  ;; => 3517401300
 
   0)
 
--- src/aoc2020/day3/core.clj
+++ src/aoc2020/day3/core.clj
@@ -6,8 +6,53 @@
 (def input (->> input-file
                 str/split-lines))
 
+(defn tree? [ ch ]
+  (= \# ch))
+
+(def part1
+  (let [pattern-len (count (first input))
+        inf-indexes (iterate #(rem (+ 3 %) pattern-len) 0)
+        slope-data  (map (fn [slope1 terrain-line]
+                           {:tree (tree? (nth terrain-line slope1))})
+                         inf-indexes input)]
+    (count (filter :tree slope-data))))
+
+(def part2
+  (let [pattern-len      (count (first input))
+        slope1-indexes   (iterate #(rem (+ 1 %) pattern-len) 0)
+        slope3-indexes   (iterate #(rem (+ 3 %) pattern-len) 0)
+        slope5-indexes   (iterate #(rem (+ 5 %) pattern-len) 0)
+        slope7-indexes   (iterate #(rem (+ 7 %) pattern-len) 0)
+        slope1-2-indexes (iterate #(rem (+ 0.5 %) pattern-len) 0)
+        slope-data       (map (fn [slope1 slope3 slope5 slope7 slope1-2 terrain-line]
+                                {:tree1   (tree? (nth terrain-line slope1))
+                                 :tree3   (tree? (nth terrain-line slope3))
+                                 :tree5   (tree? (nth terrain-line slope5))
+                                 :tree7   (tree? (nth terrain-line slope7))
+                                 :tree1-2 (when (zero? (rem slope1-2 1))
+                                            (tree? (nth terrain-line slope1-2)))})
+                              slope1-indexes
+                              slope3-indexes
+                              slope5-indexes
+                              slope7-indexes
+                              slope1-2-indexes
+                              input)]
+    (* (count (filter :tree1 slope-data))
+       (count (filter :tree3 slope-data))
+       (count (filter :tree5 slope-data))
+       (count (filter :tree7 slope-data))
+       (count (filter :tree1-2 slope-data)))))
+
+
+
 (comment
 
   input
+
+  part1
+  ;; => 223
+
+  part2
+  ;; => 3517401300
 
   0)
