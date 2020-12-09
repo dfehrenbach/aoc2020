6902096c9403df7abc726003489287db39b8bc80
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2020-12-08 20:14:29 -0600

    Setup Day 9

---
+++ src/aoc2020/day9/core.clj
@@ -0,0 +1,13 @@
+(ns aoc2020.day9.core
+  (:require [clojure.string :as string]))
+
+
+(def input-file (slurp "./src/aoc2020/day9/resources/input1.txt"))
+
+(def input (->> input-file
+                string/split-lines))
+
+(comment
+
+
+  0)
